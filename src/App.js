b50fb38d8bbe188681f83645aabbb8683969ae90
asharanjith <ashasvijayan@gmail.com>
2023-05-26 22:34:25 +0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/navbarList/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
@@ -12,6 +14,13 @@ const App = () => (
     <Skills />
     <Projects />
     <Contact />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   </div>
 );
 
