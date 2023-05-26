eb9a5b0eb5b8d10a3e65bacbc61b40c19ed74ef4
asharanjith <ashasvijayan@gmail.com>
2023-05-26 16:57:15 +0400

    Add folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <h1>My recent works</h1>
+  </div>
+);
+
+export default Projects;
